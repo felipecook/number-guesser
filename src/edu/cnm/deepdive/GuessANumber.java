9d41c582d57a7d8c707fe7c51cd5e4de7e80a0ee
Felipe Cook <felipepcook@gmail.com>
2019-01-24 16:32:13 -0700

    Refactoring with decomposition.

--- src/edu/cnm/deepdive/GuessANumber.java
+++ src/edu/cnm/deepdive/GuessANumber.java
@@ -10,41 +10,41 @@ public class GuessANumber {
   private static Scanner input = new Scanner(System.in);
 
   public static void main(String[] args) {
-    boolean playAgain;
 
+   while (playOneRound());
+  }
+
+  private static boolean playOneRound() {
+    int randomized;
+
+    randomized = random.nextInt(100) + 1;
+    System.out.println("Please input a number between 1 and 100. Press 0 to give up.");
+    while (checkGuess(randomized));
+
+    System.out.println("Do you want to play again? Yes or no");
+    String answer;
     do {
-      boolean guessAgain;
-      int guess;
-      int randomized;
+      answer = input.nextLine().trim().toLowerCase();
+    } while (answer.isEmpty());
 
-      randomized = random.nextInt(100) + 1;
-      System.out.println("Please input a number between 1 and 100. Press 0 to give up.");
-      do {
-        guess = input.nextInt();
-        if (guess == 0) {
-          guessAgain = false;
-        } else if (guess < randomized) {
-          System.out.println("That's too low");
-          guessAgain = true;
-        } else if (guess > randomized) {
-          System.out.println("That's too high");
-          guessAgain = true;
-        } else {
+    return answer.charAt(0) == 'y' ;
+  }
 
-          System.out.println("That's right!");
-          guessAgain = false;
-        }
+  private static boolean checkGuess(int randomized) {
+    boolean guessAgain = true;
+    int guess;
+    guess = input.nextInt();
+    if (guess == 0) {
+      guessAgain = false;
+    } else if (guess < randomized) {
+      System.out.println("That's too low");
+    } else if (guess > randomized) {
+      System.out.println("That's too high");
+    } else {
 
-
-      } while (guessAgain);
-      System.out.println("Do you want to play again? Yes or no");
-      String answer;
-      do {
-        answer = input.nextLine().trim().toLowerCase();
-      } while (answer.isEmpty());
-
-      playAgain = answer.charAt(0) == 'y' ;
-
-    } while (playAgain);
+      System.out.println("That's right!");
+      guessAgain = false;
+    }
+    return guessAgain;
   }
 }
